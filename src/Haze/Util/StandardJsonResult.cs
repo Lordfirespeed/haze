3d30a5406b9094391630afde60169b1b8e9a3aa9
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-07-26 18:39:39 +0100

    helper type for json responses which include HTTP status/message

---
+++ src/Haze/Util/StandardJsonResult.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace Haze.Util;
+
+public class StandardJsonResult : JsonResult
+{
+    private const int DefaultStatusCode = 200;
+
+    private readonly ObjectWrapper _wrapper;
+
+    public StandardJsonResult(object? value, object? serializerSettings = null) : base(null, serializerSettings)
+    {
+        _wrapper = new() {
+            Data = value,
+        };
+        StatusCode = DefaultStatusCode;
+        Value = _wrapper;
+    }
+
+    public new int StatusCode {
+        get => _wrapper.StatusCode ?? throw new InvalidOperationException();
+        set {
+            base.StatusCode = value;
+            _wrapper.StatusCode = value;
+            _wrapper.Message = ReasonPhrases.GetReasonPhrase(value);
+        }
+    }
+
+    public string? Detail {
+        get => _wrapper.Detail;
+        set => _wrapper.Detail = value;
+    }
+
+    class ObjectWrapper
+    {
+        [JsonPropertyName("status")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? StatusCode { get; set; }
+
+        [JsonPropertyName("message")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? Message { get; set; }
+
+        [JsonPropertyName("detail")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? Detail { get; set; }
+
+        [JsonPropertyName("data")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public object? Data { get; set; }
+    }
+}
