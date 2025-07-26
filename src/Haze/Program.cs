6abe20d508d5d2f8caf7f37ea5bcd51f1503c177
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-07-26 18:39:04 +0100

    ASP.Net app boilerplate

--- src/Haze/Program.cs
+++ src/Haze/Program.cs
@@ -1,3 +1,29 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateSlimBuilder();
+builder.WebHost.ConfigureKestrel(options => {
+    options.ListenAnyIP(5000);
+});
+builder.Services.AddControllers();
+
+var app = builder.Build();
+app.UseWebSockets(new WebSocketOptions {
+    KeepAliveInterval = TimeSpan.FromMinutes(2),
+});
+app.MapControllers();
+
+var cancellationSource = new CancellationTokenSource();
+var cancel = (PosixSignalContext ctx) => cancellationSource.Cancel();
+using (PosixSignalRegistration.Create(PosixSignal.SIGINT, cancel))
+using (PosixSignalRegistration.Create(PosixSignal.SIGTERM, cancel)) {
+    var runTask = app.RunAsync(cancellationSource.Token);
+    await runTask;
+}
