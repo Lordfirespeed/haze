24a93ad86ac00fe375c027db0059924b46749879
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-09-23 17:45:10 +0100

    fix: Npgsql's connection string has a different format

--- src/Haze/HazeDbContext.cs
+++ src/Haze/HazeDbContext.cs
@@ -26,7 +26,7 @@ public class HazeDbContext : DbContext
     );
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseNpgsql("postgresql://haze:haze@localhost:5432/haze?schema=public");
+        => options.UseNpgsql("Host=localhost; Port=5432; Username=haze; Password=haze; Database=haze;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
