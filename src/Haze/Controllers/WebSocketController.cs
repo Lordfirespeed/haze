5549fa411ccfb8ed85d36c322de67c25ccc75c9b
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2026-03-30 15:32:59 +0100

    feat: receive structured messages

--- src/Haze/Controllers/WebSocketController.cs
+++ src/Haze/Controllers/WebSocketController.cs
@@ -1,7 +1,10 @@
 using System;
+using System.IO;
 using System.Net.WebSockets;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Haze.Messages;
 using Haze.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,7 +15,13 @@ namespace Haze.Controllers;
 [ApiController]
 public class WebSocketController : HazeControllerBase<WebSocketController>
 {
+    private static readonly JsonSerializerOptions MessageSerializeOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
+    };
+
     public WebSocketController(ILogger<WebSocketController> logger) : base(logger) { }
+
     [Route("/api/websocket")]
     [HttpGet, HttpConnect]
     public async Task<IActionResult> Connect()
@@ -21,31 +30,59 @@ public class WebSocketController : HazeControllerBase<WebSocketController>
             return Problem("Only websocket requests are allowed", statusCode: StatusCodes.Status400BadRequest);
 
         using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
-        await Echo(webSocket);
+        await Handle(webSocket);
         return Empty;
     }
 
-    private static async Task Echo(WebSocket webSocket, CancellationToken ct = default)
+    private async Task Handle(WebSocket webSocket, CancellationToken ct = default)
+    {
+        HazeMessage? message;
+
+        while (true)
+        {
+            message = await ReceiveMessage(webSocket, ct);
+            if (message is null) return;
+            _logger.LogInformation("is it an auth message: {}", message is HazeAuthenticateMessage);
+        }
+    }
+
+    private async Task<HazeMessage?> ReceiveMessage(WebSocket webSocket, CancellationToken ct = default)
     {
         var buffer = new byte[1024 * 4];
-        var receiveResult = await webSocket.ReceiveAsync(
-            new ArraySegment<byte>(buffer), ct);
-
-        while (!receiveResult.CloseStatus.HasValue)
+        int totalReceived = 0;
+        while (true)
         {
-            await webSocket.SendAsync(
-                new ArraySegment<byte>(buffer, 0, receiveResult.Count),
-                receiveResult.MessageType,
-                receiveResult.EndOfMessage,
-                ct);
+            if (totalReceived >= buffer.Length)
+            {
+                await webSocket.CloseAsync(WebSocketCloseStatus.MessageTooBig, "Single message too large - maximum allowed size 4KiB", ct);
+                return null;
+            }
+            var receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer, totalReceived, buffer.Length - totalReceived), ct);
+            totalReceived += receiveResult.Count;
+            if (receiveResult.CloseStatus.HasValue)
+            {
+                await webSocket.CloseAsync(receiveResult.CloseStatus.Value, receiveResult.CloseStatusDescription, ct);
+                return null;
+            }
+            if (receiveResult.MessageType is not WebSocketMessageType.Text)
+            {
+                await webSocket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "Only messages of type 'text' containing JSON data are acceptable", ct);
+                return null;
+            }
 
-            receiveResult = await webSocket.ReceiveAsync(
-                new ArraySegment<byte>(buffer), ct);
+            if (receiveResult.EndOfMessage) break;
         }
 
-        await webSocket.CloseAsync(
-            receiveResult.CloseStatus.Value,
-            receiveResult.CloseStatusDescription,
-            ct);
+        HazeMessage? message = null;
+        try {
+            var messageStream = new MemoryStream(buffer, 0, totalReceived);
+            message = await JsonSerializer.DeserializeAsync<HazeMessage>(messageStream, MessageSerializeOptions, ct);
+        }
+        catch (JsonException exception)
+        {
+            _logger.LogDebug(exception, "Closing connection as message contained malformed JSON contents");
+            await webSocket.CloseAsync(WebSocketCloseStatus.PolicyViolation, "Only messages of type 'text' containing JSON data are acceptable", ct);
+        }
+        return message;
     }
 }
