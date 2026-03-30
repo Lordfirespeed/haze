dd8c89b5de32582d9056aae8bcd258aa10e40c37
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2026-03-30 15:31:23 +0100

    add script to help testing websocket

---
+++ scripts/test-websocket.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const foo = new WebSocket("ws://localhost:5000/api/websocket")
+
+function afterOpen() {
+  console.log("heck yeah we open")
+  foo.send(JSON.stringify({"$type": "authenticate", "client_id": "foo", "client_secret": "bar"}))
+}
+
+foo.addEventListener("message", function(event) {
+  console.log(`message: ${event.data}`);
+})
+foo.addEventListener("close", function(event) {
+  console.log(`close: ${event.data}`)
+  foo.close()
+})
+foo.addEventListener("error", function(event) {
+  console.log(`error: ${event.data}`)
+})
+
+foo.addEventListener("open", (event) => afterOpen());
+if (foo.readyState === foo.OPEN) afterOpen();
+
+try {
+  await new Promise(() => {})
+} finally {
+  foo.close()
+}
