edccec287779a70569639d8965d7e54fc57ccfe2
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2026-03-31 21:02:04 +0100

    remove `Required` modifier from `ClientId`

--- src/Haze/Models/HazeClientSession.cs
+++ src/Haze/Models/HazeClientSession.cs
@@ -10,9 +10,8 @@ public class HazeClientSession
 {
     public Guid SessionId { get; set; }
 
-    [Required]
-    public string ClientId { get; set; } = null!;
+    public string? ClientId { get; set; }
 
     [ForeignKey(nameof(ClientId))]
-    public HazeClient Client { get; set; } = null!;
+    public HazeClient? Client { get; set; }
 }
