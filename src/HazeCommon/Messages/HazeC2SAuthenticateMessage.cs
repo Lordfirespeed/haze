10c869134413911522924d153cac2ada50bccfb6
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2026-03-31 11:23:27 +0100

    mark `ClientId`, `ClientSecret` as `required`

--- src/HazeCommon/Messages/HazeC2SAuthenticateMessage.cs
+++ src/HazeCommon/Messages/HazeC2SAuthenticateMessage.cs
@@ -2,7 +2,7 @@ namespace HazeCommon.Messages;
 
 public class HazeC2SAuthenticateMessage : HazeC2SMessage
 {
-    public string ClientId { get; set; }
+    public required string ClientId { get; set; }
 
-    public string ClientSecret { get; set; }
+    public required string ClientSecret { get; set; }
 }
