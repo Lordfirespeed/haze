6156c36e956b8bd4b9ad2486960128f59552b17a
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2026-04-01 11:28:29 +0100

    fix: use `CancellationToken.None` where apt

--- src/Haze/Util/HazeWebSocket.cs
+++ src/Haze/Util/HazeWebSocket.cs
@@ -37,23 +37,23 @@ public class HazeWebSocket
         var messageStream = await ReceiveRawMessage(ct);
         if (messageStream is null) return null;
 
-        HazeC2SMessage? message = null;
+        HazeC2SMessage? message;
         try {
             message = await JsonSerializer.DeserializeAsync<HazeC2SMessage>(messageStream, MessageSerializeOptions, ct);
         }
         catch (JsonException exception)
         {
-            await using var sendScope = await _sendLock.EnterScope();
+            await using var sendScope = await _sendLock.EnterScope(CancellationToken.None);
             Logger.LogDebug(exception, "Closing connection as message contained malformed JSON contents");
-            await WebSocket.CloseAsync(WebSocketCloseStatus.PolicyViolation, "Only messages of type 'text' containing a single JSON-encoded object are acceptable", ct);
+            await WebSocket.CloseAsync(WebSocketCloseStatus.PolicyViolation, "Only messages of type 'text' containing a single JSON-encoded object are acceptable", CancellationToken.None);
             return null;
         }
 
         if (message is null || message.GetType() == typeof(HazeC2SMessage))
         {
-            await using var sendScope = await _sendLock.EnterScope();
+            await using var sendScope = await _sendLock.EnterScope(CancellationToken.None);
             Logger.LogDebug("Closing connection as message is empty");
-            await WebSocket.CloseAsync(WebSocketCloseStatus.PolicyViolation, "Empty messages (i.e. 'null', '{}') are not acceptable", ct);
+            await WebSocket.CloseAsync(WebSocketCloseStatus.PolicyViolation, "Empty messages (i.e. 'null', '{}') are not acceptable", CancellationToken.None);
             return null;
         }
 
@@ -62,29 +62,29 @@ public class HazeWebSocket
 
     public async Task<MemoryStream?> ReceiveRawMessage(CancellationToken ct = default)
     {
-        await using var receiveScope = await _receiveLock.EnterScope();
+        await using var receiveScope = await _receiveLock.EnterScope(ct);
         var buffer = new byte[1024 * 4];
         int totalReceived = 0;
         while (true)
         {
             if (totalReceived >= buffer.Length)
             {
-                await using var sendScope = await _sendLock.EnterScope();
-                await WebSocket.CloseAsync(WebSocketCloseStatus.MessageTooBig, "Single message too large - maximum allowed size 4KiB", ct);
+                await using var sendScope = await _sendLock.EnterScope(CancellationToken.None);
+                await WebSocket.CloseAsync(WebSocketCloseStatus.MessageTooBig, "Single message too large - maximum allowed size 4KiB", CancellationToken.None);
                 return null;
             }
             var receiveResult = await WebSocket.ReceiveAsync(new ArraySegment<byte>(buffer, totalReceived, buffer.Length - totalReceived), ct);
             totalReceived += receiveResult.Count;
             if (receiveResult.CloseStatus.HasValue)
             {
-                await using var sendScope = await _sendLock.EnterScope();
-                await WebSocket.CloseAsync(receiveResult.CloseStatus.Value, receiveResult.CloseStatusDescription, ct);
+                await using var sendScope = await _sendLock.EnterScope(CancellationToken.None);
+                await WebSocket.CloseAsync(receiveResult.CloseStatus.Value, receiveResult.CloseStatusDescription, CancellationToken.None);
                 return null;
             }
             if (receiveResult.MessageType is not WebSocketMessageType.Text)
             {
-                await using var sendScope = await _sendLock.EnterScope();
-                await WebSocket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "Only messages of type 'text' containing JSON data are acceptable", ct);
+                await using var sendScope = await _sendLock.EnterScope(CancellationToken.None);
+                await WebSocket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "Only messages of type 'text' containing JSON data are acceptable", CancellationToken.None);
                 return null;
             }
 
