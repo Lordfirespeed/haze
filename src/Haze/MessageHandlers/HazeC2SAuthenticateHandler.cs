d87ab7126e57f16fdb976f8cc88648c3ddadcba5
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2026-04-01 19:34:53 +0100

    it starts to look like a real handler!

--- src/Haze/MessageHandlers/HazeC2SAuthenticateHandler.cs
+++ src/Haze/MessageHandlers/HazeC2SAuthenticateHandler.cs
@@ -10,9 +10,18 @@ public class HazeC2SAuthenticateHandler : HazeC2SMessageHandler<HazeC2SAuthentic
 {
     public HazeC2SAuthenticateHandler(HazeDbContext dbContext) : base(dbContext) { }
 
-    public override Task Handle(HazeWebSocket webSocket, HazeC2SAuthenticateMessage message, CancellationToken ct = default)
+    public override async Task Handle(HazeWebSocket webSocket, HazeC2SAuthenticateMessage message, CancellationToken ct = default)
     {
-        webSocket.Logger.LogInformation("wahoo we handling");
-        return Task.CompletedTask;
+        if (webSocket.ClientSession is null) {
+            webSocket.Logger.LogInformation("session not initialised");
+            return;
+        }
+        var client = await DbContext.HazeClients.FindAsync([message.ClientId], ct);
+        if (client == null) {
+            webSocket.Logger.LogInformation("client does not exist");
+            return;
+        }
+        webSocket.ClientSession.ClientId = client.ClientId;
+        await DbContext.SaveChangesAsync(ct);
     }
 }
