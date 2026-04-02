9e2e990fe387ba0f1c5905f4412173984da7dbaf
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2026-04-02 21:33:24 +0100

    finish implementing authentication
    actually check the client secret lol

--- src/Haze/MessageHandlers/HazeC2SAuthenticateHandler.cs
+++ src/Haze/MessageHandlers/HazeC2SAuthenticateHandler.cs
@@ -1,5 +1,9 @@
+using System.Net.WebSockets;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Haze.Models;
 using Haze.Util;
 using HazeCommon.Messages;
 using Microsoft.Extensions.Logging;
@@ -8,17 +12,52 @@ namespace Haze.MessageHandlers;
 
 public class HazeC2SAuthenticateHandler : HazeC2SMessageHandler<HazeC2SAuthenticateMessage>
 {
+    // from `openssl rand -base64 48`
+    private const string FallbackClientSecret = "nwlbImS3YZRFrFIirWAeZEwNkEUt4W9uUaeBUmuQKltDNy/6CJjzP3bJ5cai7PZQ";
+
     public HazeC2SAuthenticateHandler(HazeDbContext dbContext, ILogger logger) : base(dbContext, logger) { }
+
+    private bool FixedTimeCompareClientSecret(string userClientSecret, HazeClient? client)
+    {
+        userClientSecret = userClientSecret.Normalize();
+        var trueClientSecret = client?.ClientSecret.Normalize() ?? FallbackClientSecret;
+        var trueClientSecretBytes = Encoding.UTF8.GetBytes(trueClientSecret);
+        if (userClientSecret.Length != trueClientSecret.Length) {
+            CryptographicOperations.FixedTimeEquals(trueClientSecretBytes, trueClientSecretBytes);
+            return false;
+        }
+        var userClientSecretBytes = Encoding.UTF8.GetBytes(userClientSecret);
+        return CryptographicOperations.FixedTimeEquals(userClientSecretBytes, trueClientSecretBytes);
+    }
 
     public override async Task Handle(HazeC2SAuthenticateMessage message, HazeMessageHandlerContext context, CancellationToken ct = default)
     {
         if (context.Session is null) {
-            context.Logger.LogInformation("session not initialised");
+            await context.QueueS2CMessage(new HazeS2CErrorMessage
+            {
+                RegardingMessageId = message.MessageId,
+                ErrorCode = "ERR_NO_SESSION",
+                ErrorTitle = "Bad Message",
+                ErrorDetail = "Session not initialised",
+            }, ct);
             return;
         }
         var client = await DbContext.HazeClients.FindAsync([message.ClientId], ct);
-        if (client == null) {
-            context.Logger.LogInformation("client does not exist");
+        var secretMatches = FixedTimeCompareClientSecret(message.ClientSecret, client);
+
+        if (client is null || !secretMatches) {
+            await context.CloseConnection(
+                new HazeS2CErrorMessage
+                {
+                    RegardingMessageId =  message.MessageId,
+                    ErrorCode = "ERR_INVALID_CREDENTIALS",
+                    ErrorTitle = "Unauthorized",
+                    ErrorDetail = "Invalid credentials",
+                },
+                WebSocketCloseStatus.PolicyViolation,
+                "Failed to authenticate",
+                ct
+            );
             return;
         }
         context.Session.ClientId = client.ClientId;
