a9503c5cee14331ff547bfaf745a93496596d205
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2026-04-21 08:19:45 +0100

    add `CredentialUsage` member to qr auth message

--- src/HazeCommon/Messages/Steam/HazeC2SSteamQrAuthMessage.cs
+++ src/HazeCommon/Messages/Steam/HazeC2SSteamQrAuthMessage.cs
@@ -1,6 +1,8 @@
+using HazeCommon.Models;
+
 namespace HazeCommon.Messages.Steam;
 
 public class HazeC2SSteamQrAuthMessage: HazeC2SMessage
 {
-
+    public SteamAccountCredentialUsage CredentialUsage { get; set; }
 }
